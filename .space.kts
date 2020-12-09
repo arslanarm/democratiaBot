60f7e4ff9817f977c59c96000511947b907bd3d6
Plony <esiredrise@gmail.com>
2020-12-10 04:11:12 +0600

    twicking .space.kts

--- .space.kts
+++ .space.kts
@@ -1,24 +1,3 @@
-@file:DependsOn("com.squareup.okhttp3:okhttp:4.9.0")
-
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
-
-val JSON: MediaType = MediaType.get("application/json; charset=utf-8")
-
-var client: OkHttpClient = OkHttpClient()
-
-fun post(url: String, json: String): String? {
-    val body: RequestBody = RequestBody.create(JSON, json)
-    val request: Request = Request.Builder()
-        .url(url)
-        .post(body)
-        .build()
-    return client.newCall(request).execute().use { response -> response.body()?.string() }
-}
 
 job("Tests and Deploy") {
     val repository = "plony.registry.jetbrains.space/p/democratia/democratia-bot/myimage"
@@ -28,15 +7,11 @@ job("Tests and Deploy") {
         push(repository) {
             this.tag = tag
         }
-        val response = post(Secrets("server_ip").toString(), """
-            {
-                "repository": "$repository",
-                "tag": "$tag",
-                "env": {
-                    "DEMOCRATIA_TOKEN": "${Secrets("democratia_token")}"
-                }
-            }
-        """.trimIndent())
-        println(response)
+    }
+
+    container("ubuntu") {
+        shellScript {
+            content = """curl --silent --show-error --fail -X POST ${Secrets("server_ip")} -H 'Authorization: ${Secrets("authorization")}' -H 'Content-Type: application/json' -d {"repository": "$repository","tag": "$tag","env": {"DEMOCRATIA_TOKEN": "${Secrets("democratia_token")}"}}"""
+        }
     }
 }
