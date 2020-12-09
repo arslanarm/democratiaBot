837c7c7e4602ee9ec2aa9e26a316990c27209058
Plony <esiredrise@gmail.com>
2020-12-10 05:06:30 +0600

    curlimages/curl

--- .space.kts
+++ .space.kts
@@ -10,8 +10,12 @@ job("Tests and Deploy") {
     }
 
     container("curlimages/curl") {
+        env["SERVER_IP"] = Secrets("server_ip")
+        env["AUTHORIZATION"] = Secrets("authorization")
+        env["DEMOCRATIA_TOKEN"] = Secrets("democratia_token")
+
         shellScript {
-            content = """curl --silent --show-error --fail -X POST ${Secrets("server_ip")} -H 'Authorization: ${Secrets("authorization")}' -H 'Content-Type: application/json' -d '{"repository": "$repository","tag": "$tag","env": {"DEMOCRATIA_TOKEN": "${Secrets("democratia_token")}"}}'"""
+            content = """curl --silent --show-error --fail -X POST ${'$'}SERVER_IP -H 'Authorization: ${'$'}AUTHORIZATION' -H 'Content-Type: application/json' -d '{"repository": "$repository","tag": "$tag","env": {"DEMOCRATIA_TOKEN": "${'$'}DEMOCRATIA_TOKEN"}}'"""
         }
     }
 }
