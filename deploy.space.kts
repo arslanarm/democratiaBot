c1082268057a47cea5a5fb08aa949aaf1619cc13
Arslan Armanuly <esiredrise@gmail.com>
2020-12-09 17:35:03 +0000

    another

---
+++ deploy.space.kts
@@ -0,0 +1,16 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Deploy") {
+   	docker {
+    	build {}
+       	
+        publish("plony.registry.jetbrains.space/p/democratia/democratia-bot/myimage") {
+            tag = "\$JB_SPACE_GIT_BRANCH"
+        }
+    }
+}
+
