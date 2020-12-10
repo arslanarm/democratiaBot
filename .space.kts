4d73b861593d27862bb8b72b41e4c247ccabf726
Arslan Armanuly <esiredrise@gmail.com>
2020-12-10 11:49:41 +0000

    test

--- .space.kts
+++ .space.kts
@@ -1,9 +1,22 @@
 
 job("Tests and Deploy") {
+	val repository = "plony.registry.jetbrains.space/p/democratia/democratia-bot/myimage"
+    val tag = "0.1"
 	docker {
     	build {}
-        push("plony.registry.jetbrains.space/p/democratia/democratia-bot/myimage") {
-            this.tag = "0.1"
+        push(repository) {
+            this.tag = tag
+        }
+    }
+
+    container("curlimages/curl") {
+        env["SERVER_IP"] = Secrets("server_ip")
+        env["AUTHORIZATIONTOKEN"] = Secrets("authorization")
+        env["DEMOCRATIATOKEN"] = Secrets("democratia_token")
+
+        shellScript {
+
+            content = """curl --silent --show-error --fail -X POST ${'$'}SERVER_IP -H "Authorization: ${'$'}{AUTHORIZATIONTOKEN"} -H 'Content-Type: application/json' -d '{"repository": "$repository","tag": "$tag","env": {"DEMOCRATIA_TOKEN": "${'$'}{DEMOCRATIATOKEN}"}}'"""
         }
     }
 }
