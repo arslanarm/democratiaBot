a52ac45e3f3f6f3f4acb9f525a0773fd0fbe9d0a
Plony <esiredrise@gmail.com>
2020-12-10 15:49:17 +0600

    curlimages/curl

--- .space.kts
+++ .space.kts
@@ -11,11 +11,11 @@ job("Tests and Deploy") {
 
     container("curlimages/curl") {
         env["SERVER_IP"] = Secrets("server_ip")
-        env["AUTHORIZATION"] = Secrets("authorization")
+        env["AUTHORIZATION_TOKEN"] = Secrets("authorization")
         env["DEMOCRATIA_TOKEN"] = Secrets("democratia_token")
 
         shellScript {
-            content = """curl --silent --show-error --fail -X POST ${'$'}SERVER_IP -H 'Authorization: ${'$'}AUTHORIZATION' -H 'Content-Type: application/json' -d '{"repository": "$repository","tag": "$tag","env": {"DEMOCRATIA_TOKEN": "${'$'}DEMOCRATIA_TOKEN"}}'"""
+            content = """curl --silent --show-error --fail -X POST ${'$'}SERVER_IP -H "Authorization: ${'$'}AUTHORIZATION_TOKEN" -H 'Content-Type: application/json' -d '{"repository": "$repository","tag": "$tag","env": {"DEMOCRATIA_TOKEN": "${'$'}DEMOCRATIA_TOKEN"}}'"""
         }
     }
 }
